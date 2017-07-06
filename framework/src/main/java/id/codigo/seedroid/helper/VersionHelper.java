7550112ed19c334af2eedea3483b06196f77031e
rama <rama@codigo.id>
2017-07-06 18:38:53 +0700

    rename param and add builder pattern for update version

--- framework/src/main/java/id/codigo/seedroid/helper/VersionHelper.java
+++ framework/src/main/java/id/codigo/seedroid/helper/VersionHelper.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
 
+import id.codigo.seedroid.SeedroidApplication;
 import id.codigo.seedroid.configs.RestConfigs;
 import id.codigo.seedroid.model.json.version.BaseVersionModel;
 import id.codigo.seedroid.service.ServiceListener;
@@ -14,19 +15,20 @@ import id.codigo.seedroid.service.ServiceListener;
  */
 
 public class VersionHelper {
-    private Context CONTEXT;
-    private String APP_VERSION;
-    private String APP_ID;
-    public VersionHelper(final Context CONTEXT, String APP_VERSION, String APP_ID){
-        this.CONTEXT = CONTEXT;
-        this.APP_VERSION = APP_VERSION;
-        this.APP_ID = APP_ID;
+    private Context context;
+    private String appVersion;
+    private String appId;
+
+    public VersionHelper(String appVersion, String appId){
+        this.context = SeedroidApplication.getInstance();
+        this.appVersion = appVersion;
+        this.appId = appId;
         getVersionApps(new ServiceListener<BaseVersionModel>() {
             @Override
             public void onSuccess(BaseVersionModel data) {
                 if(data.getStatus().toString().equalsIgnoreCase("200")) {
                     if (data.getMetadata().getForceUpdate() == true) {
-                        CONTEXT.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(data.getMetadata().getLinkUpdate())));
+                        context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(data.getMetadata().getLinkUpdate())));
                     } else {
                         Log.d("seedDroid","no update version!");
                     }
@@ -45,12 +47,24 @@ public class VersionHelper {
         HttpHelper.getInstance().get(
                 "http://10.4.3.239:8080" +
                         "/appmanager/mobile/api/v1/version?"+
-                        "app_id="+APP_ID+
-                        "&app_version="+APP_VERSION+
+                        "app_id="+appId+
+                        "&app_version="+appVersion+
                         "&app_platform=android",
                 callback
         );
 
+    }
+    public VersionHelper getInstance(){
+        this.context = SeedroidApplication.getInstance();
+        return this;
+    }
+    public VersionHelper setAppVersion(String appVersion){
+        this.appVersion = appVersion;
+        return this;
+    }
+    public VersionHelper setAppID(String appId){
+        this.appId = appId;
+        return this;
     }
 
 }
