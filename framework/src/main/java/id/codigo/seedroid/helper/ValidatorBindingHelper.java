9882c9ded881b1b1141b5f2b5d45ebd00052732b
rama <rama@codigo.id>
2017-07-21 16:06:10 +0700

    add comment description public method on validator binding helper

--- framework/src/main/java/id/codigo/seedroid/helper/ValidatorBindingHelper.java
+++ framework/src/main/java/id/codigo/seedroid/helper/ValidatorBindingHelper.java
@@ -30,24 +30,41 @@ public class ValidatorBindingHelper {
     private String failMessage;
     private String successMessage;
 
+    /**
+     * Initiate validator binding into class
+     */
     public ValidatorBindingHelper(ViewDataBinding target) {
         this.target = target;
         this.disabledViews = new HashSet<>();
     }
+
+    /**
+     *Setting custom fail message if validator not valid
+     */
     public ValidatorBindingHelper setFailMessage(String message){
         this.failMessage = message;
         return this;
     }
+
+    /**
+     * Setting custom success message if validator valid
+     */
     public ValidatorBindingHelper setSuccessMessage(String message){
         this.successMessage = message;
         return this;
     }
 
+    /**
+     * Initiate validatorlistener into project
+     */
     public void setValidationListener(ValidationListener validationListener) {
         this.validationListener = validationListener;
     }
 
-    public void toValidate() {
+    /**
+     * Validates all form with return listener
+     */
+    public void validatesCallback() {
         if (validationListener == null) throw new IllegalArgumentException("Validation listener should not be null.");
 
         if (validates()) {
@@ -65,16 +82,67 @@ public class ValidatorBindingHelper {
         }
     }
 
+    /**
+     * Validate single selected view with listener
+     */
+    public void validateCallback(View view) {
+        if (validationListener == null) throw new IllegalArgumentException("Validation listener should not be null.");
+
+        if (validate(view)) {
+            if (successMessage!=null){
+                validationListener.onValidationSuccess(successMessage);
+            }else {
+                validationListener.onValidationSuccess("");
+            }
+        } else {
+            if(failMessage!=null) {
+                validationListener.onValidationError(failMessage);
+            }else{
+                validationListener.onValidationError("");
+            }
+        }
+    }
+
+    /**
+     * Validates multiple selected view with listener
+     */
+    public <ViewType extends View> void validateListCallback(List<ViewType>view) {
+        if (validationListener == null) throw new IllegalArgumentException("Validation listener should not be null.");
+
+        if (validateList(view)) {
+            if (successMessage!=null){
+                validationListener.onValidationSuccess(successMessage);
+            }else {
+                validationListener.onValidationSuccess("");
+            }
+        } else {
+            if(failMessage!=null) {
+                validationListener.onValidationError(failMessage);
+            }else{
+                validationListener.onValidationError("");
+            }
+        }
+    }
+
+    /**
+     * Check status validate all form
+     */
     public boolean validates() {
         List<View> viewWithValidations = getViewsWithValidation();
         return isAllViewsValid(viewWithValidations);
     }
 
+    /**
+     * Check status validate single view
+     */
     public boolean validate(View view) {
         List<View> viewWithValidations = getViewsWithValidation(view);
         return isAllViewsValid(viewWithValidations);
     }
 
+    /**
+     * Check status validate multiple view
+     */
     public <ViewType extends View> boolean validateList(List<ViewType> views) {
         List<View> viewWithValidations = getViewsWithValidation(views);
         return isAllViewsValid(viewWithValidations);
@@ -101,18 +169,30 @@ public class ValidatorBindingHelper {
         return disabledViews.contains(rule.getView()) || rule.validate();
     }
 
+    /**
+     * Disable validation selected view
+     */
     public void disableValidation(View view) {
         disabledViews.add(view);
     }
 
+    /**
+     * Enable validation selected view
+     */
     public void enableValidation(View view) {
         disabledViews.remove(view);
     }
 
+    /**
+     * Enable form validation
+     */
     public void enableFormValidationMode() {
         this.mode = FORM_VALIDATION_MODE;
     }
 
+    /**
+     * Enable field validation
+     */
     public void enableFieldValidationMode() {
         this.mode = FIELD_VALIDATION_MODE;
     }
@@ -132,6 +212,9 @@ public class ValidatorBindingHelper {
         return ViewTagHelper.filterViewWithTag(R.id.validator_rule, view);
     }
 
+    /**
+     * Validation listener
+     */
     public interface ValidationListener {
 
         void onValidationSuccess(String message);
