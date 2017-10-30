c9e3ea60ae2fdae55b87753e7176ff103b37d2ec
rama <rama@codigo.id>
2017-10-30 16:50:30 +0700

    add media picker module

---
+++ seedroid-mediapicker/src/main/java/id/codigo/seedroid_mediapicker/imageloader/MediaImageLoader.java
@@ -0,0 +1,12 @@
+package id.codigo.seedroid_mediapicker.imageloader;
+
+import android.net.Uri;
+import android.widget.ImageView;
+
+/**
+ * Created by papahnakal on 19/10/17.
+ */
+
+public interface MediaImageLoader {
+    void displayImage(Uri uri, ImageView imageView);
+}
